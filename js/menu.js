046309689abeb302b7137ed756c8e80ba1b30dad
Leo.fp <Leo.fp@kakaocorp.com>
2018-02-21 12:53:22 +0900

    Add nav button

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
   var menu = $('.main-menu');
+  var container = $('.menubar-container')
   var btn = $('.btn-menubar, .menubar');
   
   btn.on('click', function () {
     menu.toggleClass('menu-act');
+    container.toggleClass('menubar-act');
   });
 });
